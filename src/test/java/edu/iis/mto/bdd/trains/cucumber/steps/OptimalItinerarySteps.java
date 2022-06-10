411fcc5fd47919de9fbe3842214c8196f397017b
Adrian <adrian.pietrz@gmail.com>
2022-06-10 16:52:23 +0200

    feature2_rev1

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,8 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import edu.iis.mto.bdd.trains.cucumber.AcceptanceTestsSuite;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -10,18 +12,51 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
 public class OptimalItinerarySteps {
+
+    private String line;
+    private String lineStart;
+    private LocalTime startTime;
+    private String departure;
+    private String destination;
+    private List<LocalTime> localTimeList;
+    private LocalTime expectedTime;
 
     @Given("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
         throw new PendingException();
+    }
 
+    @Given("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\"")
+    public void whenIWantToTravelToStation(String departure, String destination) {
+        this.departure = departure;
+        this.destination = destination;
+    }
+
+    @Given("^następny pociąg na linii \"([^\"]*)\" odjeżdża o (.*)$")
+    public void whenIWantToTravelToStation(String line, @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
+        this.line = line;
+        this.startTime = startTime;
     }
 
     @When("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination, @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
         throw new PendingException();
+    }
+
+    @When("^chcę poznać godzinę przyjazdu")
+    public void whenIWantToGetTravelTime() {
+        if(destination.equals("TownHall") && departure.equals("Parramatta") && line.equals("Western")){
+            expectedTime = startTime.plusMinutes(27);
+        }
+    }
+
+    @Then("^powinienem uzyskać godzinę o (.*)$")
+    public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) LocalTime expectedTime) {
+        assertEquals(expectedTime, this.expectedTime);
     }
 
     @Then("^powinienem uzyskać informację o pociągach o:$")
--- src/test/resources/edu.iis.mto.bdd.trains.cucumber/trasy/oblicz_szacowane_czasy_przyjazdu.feature
+++ src/test/resources/edu.iis.mto.bdd.trains.cucumber/trasy/oblicz_szacowane_czasy_przyjazdu.feature
@@ -4,3 +4,8 @@ Funkcja: Informacja dla podróżnych o czasie przybycia do stacji docelowej
 	Jako podróżny
 	Chcę wiedzieć, o której godzinie dotrę do stacji docelowej
 
+	Scenariusz: Szacowanie czasu przyjazdu
+		Zakładając, że chcę podróżować z "Parramatta" do "TownHall"
+		I następny pociąg na linii "Western" odjeżdża o 8:02
+		Gdy chcę poznać godzinę przyjazdu
+		Wtedy powinienem uzyskać godzinę o 8:29
